32b5848e0b64a1966665139f9cf4a453172da05a
Palacio Matias Ezequiel <mepalacio@gcgestion.com.ar>
2022-08-02 21:34:11 -0300

    actualizado

--- cypress/pages/AmazonChallengePO/AmazonCartConfirmPO.js
+++ cypress/pages/AmazonChallengePO/AmazonCartConfirmPO.js
@@ -2,7 +2,7 @@ class AmazonCartConfirmPO{
 
     //locators
     addedToCartConfirmMessage(){
-        return cy.xpath('//*[@id="attachDisplayAddBaseAlert"]/span')
+        return cy.xpath('//*[@id="sw-atc-details-single-container"]/div[2]/div/span')
         
     }
 }
